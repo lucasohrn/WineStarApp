f37f50b196f3f43f5020475897a8110b37cbe6d1
Lucas Öhrn <lucasohrn@gmail.com>
2021-05-19 12:55:42 +0200

    Added some comments

--- WineStar/WineStar.iOS/Main.cs
+++ WineStar/WineStar.iOS/Main.cs
@@ -15,6 +15,7 @@ namespace WineStar.iOS
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
+            // Testar att göra en commit
         }
     }
 }
