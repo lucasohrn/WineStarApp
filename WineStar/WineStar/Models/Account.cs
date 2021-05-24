942bfc203fd1a95b8cd351ad4a936ca4480ab2aa
Lucas Öhrn <lucasohrn@gmail.com>
2021-05-24 14:54:20 +0200

    Started implementing LogIn and color scheme

---
+++ WineStar/WineStar/Models/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WineStar.Models
+{
+    public class Account
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public Account() { }
+        public Account (string UserName, string Password)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+    }
+}
